b1562e3fca5dfc77a963525a893fa350c6fed851
kittycat <@macymacerator@gmail.com>
2022-08-18 01:59:33 -0700

    parallax tentatively working

--- Common/src/main/java/com/falkory/arcanumapi/client/gui/AbstractBookScreen.java
+++ Common/src/main/java/com/falkory/arcanumapi/client/gui/AbstractBookScreen.java
@@ -30,7 +30,7 @@ public abstract class AbstractBookScreen extends Screen {
        // drawSprite(stack, 192, 3, 0, 0, 192, 192, new ResourceLocation("textures/gui/book.png"));
         //drawScaledSprite(stack, 0, 128, 0, 0,  192, 192, 0.25f, new ResourceLocation("textures/gui/book.png"));
         //drawString(stack, this.font, "hey lily look at this !!", 192+40, 20, 20);
-        drawString(stack, this.font, "nya", 10, 10, 256);
+        //drawString(stack, this.font, "nya", 10, 10, 256);
         super.render(stack, $$1, $$2, $$3);
     }
 
--- Common/src/main/java/com/falkory/arcanumapi/client/gui/BookTabScreen.java
+++ Common/src/main/java/com/falkory/arcanumapi/client/gui/BookTabScreen.java
@@ -1,5 +1,6 @@
 package com.falkory.arcanumapi.client.gui;
 
+import com.falkory.arcanumapi.api.ArcanumAPI;
 import com.falkory.arcanumapi.book.BookMain;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.gui.screens.Screen;
@@ -44,22 +45,37 @@ public class BookTabScreen extends AbstractBookScreen{
         GL11.glScissor(x * scale, y * scale, visibleWidth * scale, visibleHeight * scale);
         // scissors on
         GL11.glEnable(GL_SCISSOR_TEST);
-        float scrollAbleX = ((minDrawSize-visibleWidth)/2f);
-        float scrollAbleY = ((minDrawSize-visibleHeight)/2f);
-        xPan = clamp(xPan, -scrollAbleX, scrollAbleX);
-        yPan = clamp(yPan, -scrollAbleY, scrollAbleY);
+        float scrollableX = (zoomDrawSize-visibleWidth)/2f;
+        float scrollableY = (zoomDrawSize-visibleHeight)/2f;
+        xPan = clamp(xPan, -scrollableX, scrollableX);
+        yPan = clamp(yPan, -scrollableY, scrollableY);
+
+        //temp
+        float spd = 1.5f;
 
         //renderResearchBackground(stack);
         //renderEntries(stack, partialTicks);
         ResourceLocation bg = new ResourceLocation("arcana","textures/research/eldritch_bg.png");
-        drawDualScaledSprite(stack, x, y, -xPan+scrollAbleX, -yPan+scrollAbleY, width, height, minDrawSize, minDrawSize, bg);
-
+        drawDualScaledSprite(stack, x, y, xPan+scrollableX, yPan+scrollableY, width, height, zoomDrawSize, zoomDrawSize, bg);
+        //drawDualScaledSprite(stack, x, y, xPan+((minDrawSize*(zoom)-visibleWidth)/2), yPan+((minDrawSize*(zoom)-visibleHeight)/2), width, height, (int)(minDrawSize*(zoom)), (int)(minDrawSize*(zoom)), new ResourceLocation("minecraft","textures/block/glass.png"));
+        drawDualScaledSprite(stack, x, y,
+          ((xPan/scrollableX)*(zoomDrawSize*spd-visibleWidth )/2)+((minDrawSize*zoom*spd) -visibleWidth )/2f,
+          ((yPan/scrollableY)*(zoomDrawSize*spd-visibleHeight)/2)+((minDrawSize*zoom*spd) -visibleHeight)/2f,
+          width, height,
+          (int)(zoomDrawSize*spd),
+          (int)(zoomDrawSize*spd),
+          new ResourceLocation("minecraft","textures/block/glass.png"));
         // scissors off
         GL11.glDisable(GL_SCISSOR_TEST);
 
+        if(ArcanumAPI.LOG.isDebugEnabled()) {drawDebug(stack);}
+
         super.render(stack, $$1, $$2, $$3);
     }
-
+    private void drawDebug(PoseStack stack){
+        drawString(stack, this.font, "zoom:"+((Float)zoom), 0, 0, 1);
+        drawString(stack, this.font, "x:"+xPan+"   y:"+yPan,0,10,1);
+    }
 
     public float getXOffset(){
         return ((width / 2f) * (1 / zoom)) + (xPan / 2f);
@@ -69,18 +85,18 @@ public class BookTabScreen extends AbstractBookScreen{
     }
 
     @Override public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY){
-        xPan += (deltaX * ZOOM_MULTIPLIER) / zoom;
-        yPan += (deltaY * ZOOM_MULTIPLIER) / zoom;
+        xPan -= (deltaX * ZOOM_MULTIPLIER) / zoom;
+        yPan -= (deltaY * ZOOM_MULTIPLIER) / zoom;
         return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
     }
 
     @Override
-    public boolean mouseScrolled(double mouseX, double mouseY, double scroll){
+    public boolean mouseScrolled(double mouseX, double mouseY, double scroll){ //I hereby propose a new zoom schema ! 1 is as far as it can zoom out, as in 1:1. any zoom is how much it's amplified from max distance
         float amount = 1.2f;
-        if((scroll < 0 && zoom > 0.5) || (scroll > 0 && zoom < 1))
+        if((scroll < 0 && zoom > 1) || (scroll > 0 && zoom < 4))
             zoom *= scroll > 0 ? amount : 1 / amount;
-        if(zoom > 1f)
-            zoom = 1f;
+        if(zoom > 4f)
+            zoom = 4f;
         return super.mouseScrolled(mouseX, mouseY, scroll);
     }
 }
